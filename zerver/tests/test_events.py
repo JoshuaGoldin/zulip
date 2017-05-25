ee0b409574a291ecaaa59acd660efd6584df75d5
JoshuaGoldin <joshuagoldin@gmail.com>
2017-05-25 11:59:47 -0700

    5 Update

--- zerver/tests/test_events.py
+++ zerver/tests/test_events.py
@@ -1014,11 +1014,11 @@ class EventsRegisterTest(ZulipTestCase):
             )"""
         changes = test_changes.get(setting_name)
         if changes is None:
-            raise AssertionError('No test created for %s' % (name))
+            raise AssertionError('No test created for %s' % (setting_name))
         do_set_user_display_setting(self.user_profile, setting_name, changes[0])
         for change in changes[1:]:
             events = self.do_test(
-                lambda: do_set_user_display_setting(self.user_profile.realm, name, val))
+                lambda: do_set_user_display_setting(self.user_profile, setting_name, change))
             error = schema_checker('events[0]', events[0])
             self.assert_on_error(error)
         timezone_schema_checker = self.check_events_dict([
